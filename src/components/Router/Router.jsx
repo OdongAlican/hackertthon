59874d370df57e0ae889e9357839a0621fe120a3
OdongAlican <sandieo.2020@gmail.com>
2021-09-21 00:21:37 +0300

    update router

--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Login from '../Login/Login';
+import Login from '../Authentication/Login/Login';
 
 const Router = () => (
   <div>
