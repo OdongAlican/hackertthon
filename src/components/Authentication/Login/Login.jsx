68e613aecadeb6b3a6d6f090a97123906a861576
OdongAlican <sandieo.2020@gmail.com>
2021-09-21 00:19:55 +0300

    update authentication directory

---
+++ src/components/Authentication/Login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Display from '../Display/Display';
+import '../index.css';
+
+const Login = () => (
+  <div className="main-login-section">
+    <div className="welcome-section">Welcome</div>
+    <div className="display-logo-section">
+      <Display />
+    </div>
+  </div>
+);
+
+export default Login;
