ee2e5efa9a49d6bc294566f9eaaf880ef6980806
OdongAlican <sandieo.2020@gmail.com>
2021-09-21 19:35:36 +0300

    update password reset route

--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from '../Authentication/Login/Login';
+import Signup from '../Authentication/Signup/Signup';
+import PasswordForget from '../Authentication/PasswordForget/PasswordForget';
+import PasswordReset from '../Authentication/PasswordReset/PasswordReset';
 
 const Router = () => (
   <div>
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/forgetpassword" component={PasswordForget} />
+        <Route exact path="/forgetreset" component={PasswordReset} />
       </Switch>
     </BrowserRouter>
   </div>
