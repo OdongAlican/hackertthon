57ebcf4b0140a43fa94f1df2d574c2ebaa8566a2
OdongAlican <sandieo.2020@gmail.com>
2021-09-21 15:08:06 +0300

    add login constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,10 @@
 export const primaryColor = '#172AD2';
 export const pageFontFamily = 'Source Sans Pro';
-
+export const loginConstants = {
+  pageMainHeader: 'Welcome back to',
+  pageExtraHeading: 'For19',
+  pageMiniHeader: `
+    Login with your accurate 
+    information to access your account
+  `,
+};
