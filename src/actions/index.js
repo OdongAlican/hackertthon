348554e366e92ac5e77a560ab9bcaa056da7b63a
OdongAlican <sandieo.2020@gmail.com>
2021-09-30 11:15:53 +0300

    update methods

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,11 @@
-import { PostRequest } from '../utils/index';
+import { PostRequest, GetRequest } from '../utils/index';
 import {
   AUTHENTICATED, UNAUTHENTICATED,
   AUTHENTICATION_ERROR, PENDING_REQUEST,
+  TOKEN_VERIFICATION, PASSWORD_RESET,
+  PASSWORD_UPDATE, updatePassword,
   signInRoute, methods, signUpRoute,
+  verificationRoute, resetToken,
 } from './constants';
 
 export const signIn = data => async dispatch => {
@@ -28,7 +31,33 @@ export const signUp = data => async dispatch => {
 
 export const forgotPassword = data => async dispatch => {
   dispatch({ type: PENDING_REQUEST, payload: 'Loading Content' });
-  console.log(data, 'data');
+  try {
+    const response = await PostRequest(methods.post, verificationRoute, data);
+    console.log(response?.data, 'response data');
+    dispatch({ type: PASSWORD_RESET, payload: response?.data });
+  } catch (error) {
+    dispatch({ type: AUTHENTICATION_ERROR, payload: error?.message });
+  }
+};
+
+export const fetchResetToken = token => async dispatch => {
+  dispatch({ type: PENDING_REQUEST, payload: 'Loading Content' });
+  try {
+    const response = await GetRequest(methods.get, `${resetToken}/${token}`);
+    dispatch({ type: TOKEN_VERIFICATION, payload: response?.data });
+  } catch (error) {
+    dispatch({ type: AUTHENTICATION_ERROR, payload: error?.message });
+  }
+};
+
+export const resetPassword = data => async dispatch => {
+  dispatch({ type: PENDING_REQUEST, payload: 'Loading Content' });
+  try {
+    const response = await PostRequest(methods.post, updatePassword, data);
+    dispatch({ type: PASSWORD_UPDATE, payload: response?.data });
+  } catch (error) {
+    dispatch({ type: AUTHENTICATION_ERROR, payload: error?.message });
+  }
 };
 
 export const signOut = () => localStorage.removeItem('user');
