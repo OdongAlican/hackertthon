bd779bec054945cd97b87c17e63a166cb228067c
OdongAlican <sandieo.2020@gmail.com>
2021-09-30 23:04:04 +0300

    update routes

--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -4,15 +4,17 @@ import Login from '../Authentication/Login/Login';
 import Signup from '../Authentication/Signup/Signup';
 import PasswordForget from '../Authentication/PasswordForget/PasswordForget';
 import PasswordReset from '../Authentication/PasswordReset/PasswordReset';
+import Dashboard from '../Dashboard/Dashboard';
 
 const Router = () => (
   <div>
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Login} />
-        <Route exact path="/signup" component={Signup} />
-        <Route exact path="/forgetpassword" component={PasswordForget} />
-        <Route exact path="/forgetreset/:token" component={PasswordReset} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/forgetpassword" component={PasswordForget} />
+        <Route path="/forgetreset/:token" component={PasswordReset} />
+        <Route path="/dashboard" component={Dashboard} />
       </Switch>
     </BrowserRouter>
   </div>
