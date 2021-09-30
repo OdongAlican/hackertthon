9ae0cdd7b06d105229f149286b0ef1475a9071c6
OdongAlican <sandieo.2020@gmail.com>
2021-09-30 11:19:15 +0300

    update component

--- src/components/Authentication/PasswordReset/PasswordReset.jsx
+++ src/components/Authentication/PasswordReset/PasswordReset.jsx
@@ -1,14 +1,62 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { resetPasswordConstants } from '../../../constants/index';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { resetPasswordConstants, passwordResetModal } from '../../../constants/index';
 import Display from '../Display/Display';
 import {
-  AuthCard, Input, ErrorSection, Button,
+  AuthCard, Input, ErrorSection,
+  Button, ScreenModal,
 } from '../../../generics/Generics';
+import { fetchResetToken, resetPassword } from '../../../actions';
+import { authValidator } from '../../../constants/validators';
+
+const initialState = {
+  password: '',
+  confirm_password: '',
+};
 
 const PasswordReset = () => {
-  const [values, setValues] = useState({});
+  const [values, setValues] = useState(initialState);
   const [errors, setErrors] = useState({});
+  const { token } = useParams();
+  const [passwordState, setPasswordState] = useState('password');
+  const [confirmPasswordState, setConfirmPasswordState] = useState('password');
+  const [displayModal, setDisplayModal] = useState(false);
+  const dispatch = useDispatch();
+
+  const authState = useSelector(state => state.authentication);
+  const closeModal = () => setDisplayModal(false);
+
+  useEffect(() => {
+    dispatch(fetchResetToken(token));
+  }, []);
+
+  const togglePassword = value => {
+    if (value === 'password') {
+      if (passwordState === 'password') {
+        setPasswordState('text');
+      } else {
+        setPasswordState('password');
+      }
+    }
+    if (value === 'confirm_password') {
+      if (confirmPasswordState === 'password') {
+        setConfirmPasswordState('text');
+      } else {
+        setConfirmPasswordState('password');
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (Object.keys(authState.passwordMessage).length > 0) {
+      setDisplayModal(true);
+    }
+  }, [authState.passwordMessage]);
+
   const handleChange = e => {
     const { name, value } = e.target;
     setValues({
@@ -17,33 +65,67 @@ const PasswordReset = () => {
     });
   };
 
-  const submitNewInformation = () => console.log('component');
-  console.log(setErrors);
+  const submitNewInformation = () => setErrors(authValidator(values, 'reset'));
+
+  useEffect(() => {
+    const errorArray = Object.keys(errors);
+    if (errorArray.length === 1 && errorArray.includes('state')) {
+      if (Object.keys(authState.resetUser).length > 0) {
+        const data = {
+          userId: authState.resetUser.user,
+          token: authState.resetUser.token,
+          password: values.password,
+        };
+        dispatch(resetPassword(data));
+      }
+    }
+  }, [errors]);
+
   return (
     <div data-testid="forgetReset" className="main-login-section">
+      {displayModal
+      && (
+      <ScreenModal
+        icon={passwordResetModal.icon}
+        header={passwordResetModal.header}
+        textOne={passwordResetModal.textOne}
+        textTwo={passwordResetModal.textTwo}
+        closeModal={closeModal}
+      />
+      )}
       <div className="welcome-section">
         <AuthCard
           pageMainHeader={resetPasswordConstants.pageMainHeader}
           pageMiniHeader={resetPasswordConstants.pageMiniHeader}
           pageExtraHeading={resetPasswordConstants.pageExtraHeading}
         >
-          <div className="login-email-section mt-3">Email Adress</div>
-          <div className="login-email-input">
+          <div className="login-password-section mt-3">Password</div>
+          <div className="login-password-input mb-3">
             <Input
-              inputName="newpasswordemail"
-              inputType="text"
+              inputName="password"
               changeValue={handleChange}
+              inputType={passwordState}
             />
-            { errors.newpasswordemail && <ErrorSection message={errors.newpasswordemail} />}
+            {
+              passwordState === 'password'
+                ? (<i className="fas fa-eye-slash" onClick={() => togglePassword('password')} />)
+                : (<i className="fas fa-eye" onClick={() => togglePassword('password')} />)
+            }
+            { errors.password && <ErrorSection message={errors.password} />}
           </div>
-          <div className="login-email-section mt-3">Email Adress</div>
-          <div className="login-email-input">
+          <div className="login-password-section mt-3">Confirm Password</div>
+          <div className="login-password-input mb-3">
             <Input
-              inputName="newpasswordemail"
-              inputType="text"
+              inputName="confirm_password"
               changeValue={handleChange}
+              inputType={confirmPasswordState}
             />
-            { errors.newpasswordemail && <ErrorSection message={errors.newpasswordemail} />}
+            {
+              confirmPasswordState === 'password'
+                ? (<i className="fas fa-eye-slash" onClick={() => togglePassword('confirm_password')} />)
+                : (<i className="fas fa-eye" onClick={() => togglePassword('confirm_password')} />)
+            }
+            { errors.confirm_password && <ErrorSection message={errors.confirm_password} />}
           </div>
           <div className="login-button-section mt-4">
             <Button
