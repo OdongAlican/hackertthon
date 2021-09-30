9f07a1d18675a29ef9de3e6bb1ed92e50aaf7036
OdongAlican <sandieo.2020@gmail.com>
2021-09-30 11:15:17 +0300

    add action types

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -2,11 +2,17 @@ export const AUTHENTICATED = 'AUTHENTICATED_USER';
 export const UNAUTHENTICATED = 'UNAUTHENTICATED_USER';
 export const AUTHENTICATION_ERROR = 'AUTHENTICATION_ERROR';
 export const PENDING_REQUEST = 'PENDING_REQUEST';
+export const TOKEN_VERIFICATION = 'TOKEN_VERIFICATION';
+export const PASSWORD_RESET = 'PASSWORD_RESET';
+export const PASSWORD_UPDATE = 'PASSWORD_UPDATE';
 
 export const methods = {
   get: 'get', post: 'post', delete: 'delete', put: 'put',
 };
 
-export const baseUrl = 'http://localhost:8000';
-export const signUpRoute = 'api/users/register';
-export const signInRoute = 'api/users/login';
+export const baseUrl = 'http://localhost:8000/api/users';
+export const signUpRoute = 'register';
+export const signInRoute = 'login';
+export const verificationRoute = 'verificationmail';
+export const resetToken = 'passwordreset';
+export const updatePassword = 'updatepassword';
