94e53e14b746021e7540116ec4213fa659a69cc8
OdongAlican <sandieo.2020@gmail.com>
2021-09-30 11:18:35 +0300

    update component

--- src/components/Authentication/PasswordForget/PasswordForget.jsx
+++ src/components/Authentication/PasswordForget/PasswordForget.jsx
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useHistory } from 'react-router-dom';
 import { forgotPassword } from '../../../actions';
-import { forgetPasswordConstants } from '../../../constants/index';
-import { newPasswordValidator } from '../../../constants/validators';
+import { forgetPasswordConstants, forgotPasswordModal } from '../../../constants/index';
+import { authValidator } from '../../../constants/validators';
 import {
-  AuthCard, Input, Button, ErrorSection,
+  AuthCard, Input, Button,
+  ErrorSection, ScreenModal,
 } from '../../../generics/Generics';
 import Display from '../Display/Display';
 
-const initialState = { newpasswordemail: '' };
+const initialState = { userEmail: '' };
 
 const PasswordForget = () => {
   const [errors, setErrors] = useState({});
   const [values, setValues] = useState(initialState);
+  const [displayModal, setDisplayModal] = useState(false);
   const dispatch = useDispatch();
+  const history = useHistory();
+  const emailContentState = useSelector(state => state.authentication.emailContent);
+
   const handleChange = e => {
     const { name, value } = e.target;
     setValues({
@@ -23,13 +28,34 @@ const PasswordForget = () => {
     });
   };
 
-  const submitNewPassword = () => setErrors(newPasswordValidator(values));
+  const submitNewPassword = () => setErrors(authValidator(values, 'verification'));
+  const closeModal = () => setDisplayModal(false);
 
   useEffect(() => {
-    dispatch(forgotPassword(values));
+    if (Object.keys(emailContentState).length > 0) {
+      setDisplayModal(true);
+    }
+  }, [emailContentState]);
+
+  useEffect(() => {
+    const errorArray = Object.keys(errors);
+    if (errorArray.length === 1 && errorArray.includes('state')) {
+      dispatch(forgotPassword(values, history));
+    }
   }, [errors]);
   return (
     <div data-testid="forgetPassword" className="main-login-section">
+      {displayModal
+      && (
+      <ScreenModal
+        icon={forgotPasswordModal.icon}
+        header={forgotPasswordModal.header}
+        textOne={forgotPasswordModal.textOne}
+        textTwo={forgotPasswordModal.textTwo}
+        closeModal={closeModal}
+        email={emailContentState.message.to}
+      />
+      )}
       <div className="welcome-section">
         <AuthCard
           pageMainHeader={forgetPasswordConstants.pageMainHeader}
@@ -39,11 +65,11 @@ const PasswordForget = () => {
           <div className="login-email-section mt-3">Email Adress</div>
           <div className="login-email-input">
             <Input
-              inputName="newpasswordemail"
+              inputName="userEmail"
               inputType="text"
               changeValue={handleChange}
             />
-            { errors.newpasswordemail && <ErrorSection message={errors.newpasswordemail} />}
+            { errors.userEmail && <ErrorSection message={errors.userEmail} />}
           </div>
           <div className="login-button-section mt-4">
             <Button
