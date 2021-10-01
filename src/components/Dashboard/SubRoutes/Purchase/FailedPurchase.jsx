8ceaacdd502162f2bff8c2cf4835b52e9efb5a6b
OdongAlican <sandieo.2020@gmail.com>
2021-10-02 01:05:37 +0300

    update FailedPurchase component

--- src/components/Dashboard/SubRoutes/Purchase/FailedPurchase.jsx
+++ src/components/Dashboard/SubRoutes/Purchase/FailedPurchase.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { array } from '../../../../constants/index';
 
-const FailedPurchase = () => (
-  <div>
-    <p>Failed Purchase</p>
-  </div>
+const FailedPurchase = ({ Card }) => (
+  array.map(element => (
+    <Card
+      key={element.id}
+      productCardName={element.productCardName}
+      productCardSeller={element.productCardSeller}
+      productCardDate={element.productCardDate}
+      productCardAmount={element.productCardAmount}
+      ProductStatus="failed"
+    />
+  ))
 );
 
 export default FailedPurchase;
