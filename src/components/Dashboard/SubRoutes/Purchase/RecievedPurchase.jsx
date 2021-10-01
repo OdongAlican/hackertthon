64b834db7474cec1a378c3f5ee96fdd25e618a6a
OdongAlican <sandieo.2020@gmail.com>
2021-10-02 01:07:01 +0300

    update RecievedPurchase component

--- src/components/Dashboard/SubRoutes/Purchase/RecievedPurchase.jsx
+++ src/components/Dashboard/SubRoutes/Purchase/RecievedPurchase.jsx
@@ -1,49 +1,19 @@
 /* eslint-disable react/prop-types */
 
 import React from 'react';
+import { array } from '../../../../constants/index';
 
-const RecievedPurchase = ({ Card }) => {
-  const array = [
-    {
-      id: 1,
-      productCardName: 'Zara Dress',
-      productCardSeller: 'David & Sons Limited',
-      productCardDate: '20/19/2021',
-      productCardAmount: '$ 2000',
-    },
-    {
-      id: 2,
-      productCardName: 'Zara Dress',
-      productCardSeller: 'David & Sons Limited',
-      productCardDate: '20/19/2021',
-      productCardAmount: '$ 2000',
-    },
-    {
-      id: 3,
-      productCardName: 'Zara Dress',
-      productCardSeller: 'David & Sons Limited',
-      productCardDate: '20/19/2021',
-      productCardAmount: '$ 2000',
-    },
-    {
-      id: 4,
-      productCardName: 'Zara Dress',
-      productCardSeller: 'David & Sons Limited',
-      productCardDate: '20/19/2021',
-      productCardAmount: '$ 2000',
-    },
-  ];
-  return (
-    array.map(element => (
-      <Card
-        key={element.id}
-        productCardName={element.productCardName}
-        productCardSeller={element.productCardSeller}
-        productCardDate={element.productCardDate}
-        productCardAmount={element.productCardAmount}
-      />
-    ))
-  );
-};
+const RecievedPurchase = ({ Card }) => (
+  array.map(element => (
+    <Card
+      key={element.id}
+      productCardName={element.productCardName}
+      productCardSeller={element.productCardSeller}
+      productCardDate={element.productCardDate}
+      productCardAmount={element.productCardAmount}
+      ProductStatus="recieved"
+    />
+  ))
+);
 
 export default RecievedPurchase;
