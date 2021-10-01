05e8a23edf0035342b160e2d115166d52f551821
OdongAlican <sandieo.2020@gmail.com>
2021-10-02 01:47:20 +0300

    update layout

--- src/components/Dashboard/Sales.jsx
+++ src/components/Dashboard/Sales.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import {
+  useRouteMatch,
+  BrowserRouter as Router,
+} from 'react-router-dom';
+import {
+  InnerRoutesHeader,
+  InnerRoutesCardOne, InnerRoutesWrapper,
+} from '../../generics/Generics';
 
-const Sales = () => (
-  <div>
-    <p>Sales</p>
-  </div>
-);
+const Sales = () => {
+  const { url, path } = useRouteMatch();
+  return (
+    <InnerRoutesWrapper>
+      <Router>
+        <InnerRoutesHeader url={url} />
+        <InnerRoutesCardOne path={path} />
+      </Router>
+    </InnerRoutesWrapper>
+  );
+};
 
 export default Sales;
